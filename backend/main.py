b0810d9a862e2f557b4703bcde419e71f9849a05
mwichabe <mwichabecollins@gmail.com>
2024-02-22 15:33:46 +0300

    Flask SetUp

---
+++ backend/main.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_restx import Api, Resource
+from config import DevConfig
+from models import Recipe
+from exts import db
+
+app = Flask(__name__)
+app.config.from_object(DevConfig)
+db.init_app(app)
+api = Api(app, doc='/docs')
+
+@api.route('/hello')
+class HelloResources(Resource):
+    def get(self):
+        return {"message": "Hello world"}
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        "db": db,
+        "Recipe": Recipe
+    }
+
+if __name__ == '__main__':
+    app.run(debug=True)
